08ab152bb6d91eadaa51c15b3124403067733b09
aaron wertheim <aawertheim@gmail.com>
2022-08-24 15:47:11 -0500

    bg image, carousel opacity

--- client/src/ShowCard.js
+++ client/src/ShowCard.js
@@ -5,12 +5,12 @@ import {  Slide } from "pure-react-carousel";
 function ShowCard({show, user, setCurrentShow, index}) {
         return (
                 <Slide index={index} >
-                    <div className="flex flex-shrink-0 relative w-full sm:w-auto ">
-                        <img src={show.poster_url} alt="Poster" className="object-cover object-center w-full pt-32 pb-32" />
+                    <div className="flex flex-shrink-0 relative w-full sm:w-auto  ">
+                        <img src={show.poster_url} alt="Poster" className="object-cover object-center  w-full pt-32 pb-32" />
                         <div className="bg-gray-800 bg-opacity-30 absolute rounded w-full h-full px-2 pt-4 shadow-lg ">
-                            <h2 className="text-lg font-semibold leading-4 lg:leading-5 text-white absolute">{show.title}</h2>
+                            <h2 className="text-lg font-bold leading-4 lg:leading-5 text-white text-center mx-auto absolute ">{show.title}</h2>
                             <div className="flex h-full items-end pb-6 ">
-                                <div className="font-semibold leading-5 lg:leading-6 text-white  ">
+                                <div className="font-semibold leading-5 lg:leading-6 text-white bg-gray-800 bg-opacity-30 p-1 rounded  ">
                                     <h3>{show.audition_date}</h3>
                                     <input 
                                         className="bg-transparent mt-2" 

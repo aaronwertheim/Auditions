802708e74c1ea0f23249a0b8f62a525d76dcc012
aaron wertheim <aawertheim@gmail.com>
2022-08-24 19:32:39 -0500

    text-center, removed 2 shows

--- client/src/ShowCard.js
+++ client/src/ShowCard.js
@@ -8,7 +8,7 @@ function ShowCard({show, user, setCurrentShow, index}) {
                     <div className="flex flex-shrink-0 relative w-full sm:w-auto  ">
                         <img src={show.poster_url} alt="Poster" className="object-cover object-center  w-full pt-32 pb-32" />
                         <div className="bg-gray-800 bg-opacity-30 absolute rounded w-full h-full px-2 pt-4 shadow-lg ">
-                            <h2 className="text-lg font-bold leading-4 lg:leading-5 text-white text-center mx-auto absolute ">{show.title}</h2>
+                            <h2 className="text-lg font-bold leading-4 lg:leading-5 text-white mx-auto absolute ">{show.title}</h2>
                             <div className="flex h-full items-end pb-6 ">
                                 <div className="font-semibold leading-5 lg:leading-6 text-white bg-gray-800 bg-opacity-30 p-1 rounded  ">
                                     <h3>{show.audition_date}</h3>

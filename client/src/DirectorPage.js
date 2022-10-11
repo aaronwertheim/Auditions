b8ab8703c637134f1a06172bbfa5400b6213b32c
aaron wertheim <aawertheim@gmail.com>
2022-10-11 15:40:00 -0500

    carousel resize

--- client/src/DirectorPage.js
+++ client/src/DirectorPage.js
@@ -30,13 +30,13 @@ function DirectorPage({user, setCurrentShow}) {
             </div>
             <div className="flex items-center justify-center w-full h-full ">
                 <CarouselProvider naturalSlideWidth={100} isIntrinsicHeight={true} totalSlides={showsToDisplay.length} visibleSlides={1} step={1} infinite={true}>
-                    <div className="w-full relative flex items-center justify-center">
-                        {/* <ButtonBack role="button" aria-label="slide backward" className="absolute z-30 left-0 ml-8  bg-gray-800 ring-2 ring-gray-800 ring-offset-2 hover:animate-ping cursor-pointer" id="prev">
+                    <div className="w-screen lg:w-full relative flex items-center justify-center">
+                        <ButtonBack role="button" aria-label="slide backward" className="absolute z-30 left-0 ml-8  bg-gray-800 ring-2 ring-gray-800 ring-offset-2 hover:animate-ping cursor-pointer" id="prev">
                             <svg width={14} height={20} viewBox="0 0 8 14" fill="none" xmlns="http://www.w3.org/2000/svg">
                                 <path d="M7 1L1 7L7 13" stroke="white" strokeWidth={2} strokeLinecap="round" strokeLinejoin="round" />
                             </svg>
-                        </ButtonBack> */}
-                        <div className="w-full h-full mb-20 mx-auto overflow-x-hidden overflow-y-hidden">
+                        </ButtonBack>
+                        <div className="w-2/3 lg:w-full h-full mb-20 mx-auto overflow-x-hidden overflow-y-hidden">
                             <Slider>
                                 <div id="slider" className="h-full flex lg:gap-8 md:gap-6 gap-14 items-center justify-start transition ease-out duration-700">
                                   {showsToDisplay.map((show, index) => (  
@@ -45,11 +45,11 @@ function DirectorPage({user, setCurrentShow}) {
                                 </div>
                             </Slider>
                         </div>
-                        {/* <ButtonNext role="button" aria-label="slide forward" className="absolute z-30 right-0 mr-8  bg-gray-800 ring-2 ring-gray-800 ring-offset-2 hover:animate-ping cursor-pointer" id="next">
+                        <ButtonNext role="button" aria-label="slide forward" className="absolute z-30 right-0 mr-8  bg-gray-800 ring-2 ring-gray-800 ring-offset-2 hover:animate-ping cursor-pointer" id="next">
                             <svg width={14} height={20} viewBox="0 0 8 14" fill="none" xmlns="http://www.w3.org/2000/svg">
                                 <path d="M1 1L7 7L1 13" stroke="white" strokeWidth={2} strokeLinecap="round" strokeLinejoin="round" />
                             </svg>
-                        </ButtonNext> */}
+                        </ButtonNext>
                     </div>
                 </CarouselProvider>
             </div>
--- client/src/PerformerPage.js
+++ client/src/PerformerPage.js
@@ -24,13 +24,13 @@ function PerformerPage({user, setCurrentShow}) {
             </div>
             <div className="flex items-center justify-center w-full h-full">
                 <CarouselProvider naturalSlideWidth={100} isIntrinsicHeight={true} totalSlides={shows.length} visibleSlides={1} step={1} infinite={true}>
-                    <div className="w-full relative flex items-center justify-center">
+                    <div className="w-screen lg:w-full  relative flex items-center justify-center">
                         <ButtonBack role="button" aria-label="slide backward" className="absolute z-30 left-0 ml-8 bg-gray-800 ring-2 ring-gray-800 ring-offset-2 hover:animate-ping  cursor-pointer" id="prev">
                             <svg width={14} height={20} viewBox="0 0 8 14" fill="none" xmlns="http://www.w3.org/2000/svg">
                                 <path d="M7 1L1 7L7 13" stroke="white" strokeWidth={2} strokeLinecap="round" strokeLinejoin="round" />
                             </svg>
                         </ButtonBack>
-                        <div className="w-full h-full mb-20 mx-auto overflow-x-hidden overflow-y-hidden">
+                        <div className="w-2/3 lg:w-full h-full mb-20 mx-auto overflow-x-hidden overflow-y-hidden">
                             <Slider>
                                 <div id="slider" className="h-full flex lg:gap-8 md:gap-6 gap-14 items-center justify-start transition ease-out duration-700">
                                   {shows.map((show, index) => (  
